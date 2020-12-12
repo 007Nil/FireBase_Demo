b0e08febd9232a928ee77a23d927de46caa28fd3
007Nil <sagniksarkar@ymail.com>
2020-12-13 04:47:00 +0530

    Add firebase database, message send and receive functions

--- app/src/main/java/com/isabella/FirebaseDemo/MainActivity.java
+++ app/src/main/java/com/isabella/FirebaseDemo/MainActivity.java
@@ -1,18 +1,33 @@
 package com.isabella.FirebaseDemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
+    private Button sendMessage;
+    private EditText message;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logout = findViewById(R.id.logout);
+        message = findViewById(R.id.message);
+        sendMessage = findViewById(R.id.send);
+        listView = findViewById(R.id.listView);
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +49,44 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_message = message.getText().toString();
+
+                if (TextUtils.isEmpty(txt_message)){
+                    Toast.makeText(MainActivity.this, "Message Feild is Emply", Toast.LENGTH_SHORT).show();
+                }else{
+                    FirebaseDatabase.getInstance().getReference().child("Demo_Database").push().child("Message").setValue(txt_message);
+                }
+            }
+        });
+
+//        FirebaseDatabase.getInstance().getReference().child("Demo_Database").child("Message").setValue("Test Message");
+        ArrayList<String> messageList = new ArrayList<>();
+        ArrayAdapter adopter = new ArrayAdapter<String>(this, R.layout.list_item, messageList);
+        listView.setAdapter(adopter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Demo_Database");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageList.clear();
+                for (DataSnapshot each: snapshot.getChildren()){
+                    messageList.add(each.getValue().toString().replace("{", "").replace("Message=", "")
+                    .replace("}",""));
+                }
+                adopter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+
 }
